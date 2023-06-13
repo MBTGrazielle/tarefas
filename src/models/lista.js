d142981d5b8ec49cd68e211832cd8c509dcf5aca
gabrielli.melo <gabrielli.melo@simbiox.com.br>
2023-06-12 21:47:10 -0300

    chore() - Model das Listas

---
+++ src/models/lista.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const moment = require("moment");
+
+const ListaSchema = new mongoose.Schema({
+  id: {
+    type: Number,
+    default: 1,
+  },
+  createdAt: { type: Date, default: Date.now },
+  titulo: { type: String, required: true },
+});
+
+ListaSchema.set("toJSON", {
+  transform: function (doc, ret) {
+    ret.createdAt = moment(ret.createdAt).format("DD/MM/YYYY HH:mm:ss");
+    return ret;
+  },
+});
+
+module.exports = mongoose.model("Lista", ListaSchema);
