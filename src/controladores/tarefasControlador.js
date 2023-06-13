59f79a1d31b8981667e177123f02fc4c7e4d99e1
gabrielli.melo <gabrielli.melo@simbiox.com.br>
2023-06-13 10:25:51 -0300

    feat() - controlador para cadastrar tarefas em listas, tarefas em sublistas

--- src/controladores/tarefasControlador.js
+++ src/controladores/tarefasControlador.js
@@ -1,6 +1,110 @@
-const cadastrarTarefas = async (req, res) => {};
+const mongoose = require("mongoose");
+const SubListaSchema = require("../models/subLista");
+const ListaSchema = require("../models/lista");
+const TarefaSchema = require("../models/tarefa");
 
-const todasTarefas = async (req, res) => {};
+function generateAutoID() {
+  const timestamp = new Date().getTime().toString();
+  const randomNum = Math.floor(Math.random() * 1000).toString();
+  const autoID = timestamp + randomNum;
+  return autoID;
+}
+
+const cadastrarTarefasLista = async (req, res) => {
+  try {
+    const { lista_id, titulo, descricao, alocado } = req.body;
+
+    const listaExistente = await ListaSchema.findOne({ id: lista_id });
+    if (!listaExistente) {
+      return res.status(404).json({ error: "Lista não encontrada." });
+    }
+
+    const novaTarefa = new TarefaSchema({
+      id: generateAutoID(),
+      titulo,
+      lista_id: listaExistente.id,
+      descricao,
+      alocado,
+      tipo: "tarefa",
+    });
+
+    const tarefaSalva = await novaTarefa.save();
+
+    res.status(201).json(tarefaSalva);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "Erro ao cadastrar a tarefa.", erro: error.message });
+  }
+};
+
+const cadastrarTarefasSubLista = async (req, res) => {
+  try {
+    const { lista_id, sublista_id, titulo, descricao, alocado } = req.body;
+
+    const listaExistente = await ListaSchema.findOne({ id: lista_id });
+
+    const sublistaExistente = await SubListaSchema.findOne({ id: sublista_id });
+
+    if (!listaExistente) {
+      return res.status(404).json({ error: "Lista não encontrada." });
+    }
+
+    if (!sublistaExistente) {
+      return res.status(404).json({ error: "SubLista não encontrada." });
+    }
+
+    if (sublistaExistente.lista_id !== listaExistente.id) {
+      return res.status(404).json({ error: "Listas incompativeis." });
+    }
+
+    const novaTarefa = new TarefaSchema({
+      id: generateAutoID(),
+      titulo,
+      lista_id: listaExistente.id,
+      sublista_id: sublistaExistente.id,
+      descricao,
+      alocado,
+      tipo: "tarefa",
+    });
+    const tarefaSalva = await novaTarefa.save();
+    console.log(tarefaSalva);
+    res.status(201).json(tarefaSalva);
+  } catch (error) {
+    res.status(500).json({ error: "Erro ao cadastrar a tarefa." });
+  }
+};
+
+function unificarPorListaId(tarefas) {
+  const unificado = {};
+
+  tarefas.forEach((item) => {
+    const listaId = item.lista_id;
+
+    if (!unificado[listaId]) {
+      unificado[listaId] = {
+        lista_id: listaId,
+        tarefasLista: [],
+      };
+    }
+
+    unificado[listaId].tarefasLista.push(item);
+  });
+
+  return Object.values(unificado);
+}
+
+const todasTarefas = async (req, res) => {
+  try {
+    const subListas = await TarefaSchema.find();
+
+    const tarefasFormatadas = unificarPorListaId(subListas);
+
+    res.status(200).json(tarefasFormatadas);
+  } catch (error) {
+    res.status(500).json({ error: "Erro ao buscar as tarefas." });
+  }
+};
 
 const buscarTarefa = async (req, res) => {};
 
@@ -9,7 +113,8 @@ const atualizarTarefa = async (req, res) => {};
 const deletarTarefa = async (req, res) => {};
 
 module.exports = {
-  cadastrarTarefas,
+  cadastrarTarefasLista,
+  cadastrarTarefasSubLista,
   todasTarefas,
   buscarTarefa,
   atualizarTarefa,
