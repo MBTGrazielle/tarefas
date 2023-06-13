457a96bf457747d841f466c34dc01b09bb65c0e7
gabrielli.melo <gabrielli.melo@simbiox.com.br>
2023-06-12 21:51:02 -0300

    chore() - Controladores e rotas das tarefas

---
+++ src/controladores/tarefasControlador.js
@@ -0,0 +1,17 @@
+const cadastrarTarefas = async (req, res) => {};
+
+const todasTarefas = async (req, res) => {};
+
+const buscarTarefa = async (req, res) => {};
+
+const atualizarTarefa = async (req, res) => {};
+
+const deletarTarefa = async (req, res) => {};
+
+module.exports = {
+  cadastrarTarefas,
+  todasTarefas,
+  buscarTarefa,
+  atualizarTarefa,
+  deletarTarefa,
+};
---
+++ src/rotas/rotasTarefas.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+
+const { Router } = require("express");
+
+const {
+  cadastrarTarefas,
+  todasTarefas,
+  buscarTarefa,
+  atualizarTarefa,
+  deletarTarefa,
+} = require("../controladores/tarefasControlador");
+
+const rota = Router();
+
+rota.post("/cadastrarTarefas", cadastrarTarefas);
+rota.get("/todasTarefas", todasTarefas);
+rota.get("/buscarTarefas", buscarTarefa);
+rota.get("/buscarTarefa", buscarTarefa);
+rota.patch("/atualizarTarefa/:id", atualizarTarefa);
+rota.delete("/deletarTarefa/:id", deletarTarefa);
+
+module.exports = rota;
